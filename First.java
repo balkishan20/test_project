4ae780e4fc6ca65b9f39fe3de658caf6977b6af0
Balkishan Gaur <balkishangaur98@gmail.com>
2022-10-13 10:26:45 +0530

    add java new file/First.java

---
+++ First.java
@@ -0,0 +1,10 @@
+class Second {
+    static public void main(String balu[]){
+        balu[0]="hhh";
+        System.out.print("Hello Dps");
+        System.out.println(balu[0]);
+    }
+}
+class third {
+
+}
